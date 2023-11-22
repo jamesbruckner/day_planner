c7fa7553f7f58a8c842efb3535c83ab619041976
James Bruckner <jamesbruckner55@gmail.com>
2023-11-21 17:57:27 -0700

    just pushing this up even though it doesnt work anymore

---
+++ 02-Challenge/Develop/script.js
@@ -0,0 +1,75 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+// $(function () {
+//   // TODO: Add a listener for click events on the save button. This code should
+//   // use the id in the containing time-block as a key to save the user input in
+//   // local storage. HINT: What does `this` reference in the click listener
+//   // function? How can DOM traversal be used to get the "hour-x" id of the
+//   // time-block containing the button that was clicked? How might the id be
+//   // useful when saving the description in local storage?
+//   //
+//   // TODO: Add code to apply the past, present, or future class to each time
+//   // block by comparing the id to the current hour. HINTS: How can the id
+//   // attribute of each time-block be used to conditionally add or remove the
+//   // past, present, and future classes? How can Day.js be used to get the
+//   // current hour in 24-hour time?
+//   //
+//   // TODO: Add code to get any user input that was saved in localStorage and set
+//   // the values of the corresponding textarea elements. HINT: How can the id
+//   // attribute of each time-block be used to do this?
+//   //
+//   // TODO: Add code to display the current date in the header of the page.
+
+
+function updateCurrentTime() {
+  var currentTime = dayjs().format('MMM D, YYYY h:mm:ss a')
+  $('#currentDay').text(currentTime)
+}
+setInterval(updateCurrentTime, 1000);
+
+
+var textInput = document.querySelector("#hour-9 #hour textarea");
+var saveButton = document.querySelector('#saveButton');
+// var text = [];
+
+saveButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  localStorage.setItem("hour-9", JSON.stringify(textInput.value));
+});
+
+function init() {
+  var storedText = JSON.parse(localStorage.getItem('hour-9'));
+  if (storedText !== null) {
+    text = document.querySelector("#hour-9 textarea");
+    text.innerText = storedText
+  }
+  console.log(storedText)
+}
+init()
+
+function checkTime() {
+  var currentTime = dayjs().$H
+  var timeBlock =  document.querySelectorAll('.time-block')
+  for(var i = 0; i < timeBlock.length; i++) {
+    var hour = Number(timeBlock[i].id.split('-')[1])
+    console.log(currentTime)
+console.log(timeBlock[i].id.split('-')[1])
+if (currentTime < hour) {
+    styles.css.future
+  }
+  if (currentTime > hour) {
+    styles.css.past
+  }
+  else if (currentTime === hour) {
+    style.css.present
+  }}
+}
+//red present, gray past, green future
+//these are the only differences between CSS classes
+// function for current time,
+checkTime();
+//
+//document.getElementsByClassName('time-block') <-- anohter option
+
+//i was very close to getting this all to work but i had to trash everything and i blame the tutor i had. 
